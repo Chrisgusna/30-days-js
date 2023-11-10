3d5a7dc3cbaee43bfc01a986cb592a0d687104a8
Christopher John <christopherlegacy01@gmail.com>
2023-11-10 09:46:17 -0800

    finished exercise one of Day 02

--- Day-03/index.js
+++ Day-03/index.js
@@ -121,3 +121,24 @@ console.log(!(4 > 3 && 10 < 12));
 console.log(!(4 > 3 && 10 > 12));
 console.log(!(4 === '4'));
 console.log(!(4 === '4'));
+
+/*
+Use the Date object to do the following activities
+
+What is the year today?
+What is the month today as a number?
+What is the date today?
+What is the day today as a number?
+What is the hours now?
+What is the minutes now?
+Find out the numbers of seconds elapsed from January 1, 1970 to now
+*/
+let now = new Date();
+
+console.log(now.getFullYear());
+console.log(now.getMonth() + 1);
+console.log(now.getDate());
+console.log(now.getDate() + 1);
+console.log(now.getHours());
+console.log(now.getMinutes());
+console.log(now.getTime()) 
